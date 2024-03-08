b3d8cb49132e590ac5268afd3b2e7a88a6ed8e76
Marike <Mariabessolova23836@gmail.com>
2024-03-08 09:59:36 +0300

    Научилась пересылать сообщения!

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import telebot
 from telebot import types# для указание типов
 import config
 
+idd = -1002140770203
 bot = telebot.TeleBot('7165523324:AAEIvwFPprEFi5shFyyYhDCStuvRZczgU1s')
 
 @bot.message_handler(commands=['start'])
@@ -14,8 +15,16 @@ def start(message):
     mess = f'Приветик:), <b>{message.from_user.first_name} {message.from_user.last_name}</b>'
     bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
 
+
 @bot.message_handler(content_types=['text'])
 def another_commands(message):
+    if(message.text == 'Супер! Я готов!'):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Я нашёл! Дальше)")
+        markup.add(btn1)
+        bot.forward_message(message.chat.id, idd, 5)
+        mess = f'Ура, ты дошла до сюда, <b>{message.from_user.first_name} {message.from_user.last_name}</b>'
+        bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
     if(message.text == "Посмотреть основные объекты!"):
         bot.forward_message(message.chat.id, message.chat.id, message.message_id)
     elif(message.text == "Что умеет этот бот?"):
@@ -28,11 +37,20 @@ def another_commands(message):
         markup.add(btn1, btn2, btn3)
         mess = f'Ничего страшного, <b>{message.from_user.first_name} {message.from_user.last_name}</b>, в следующий раз продолжишь!'
         bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
+    elif (message.text == "Пройти квест!"):
+        kvest(message)
     else:
         mess = f'Приветик:), <b>{message.from_user.first_name} {message.from_user.last_name}</b>'
         bot.send_message(message.chat.id, mess, parse_mode='html')
 
 
-    
+def kvest(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("Супер! Я готов!")
+    btn2 = types.KeyboardButton("В другой раз")
+    markup.add(btn1, btn2)
+    mess = f'Ничего страшного, <b>{message.from_user.first_name} {message.from_user.last_name}</b>, в следующий раз продолжишь!'
+    bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
+
 
 bot.polling(none_stop=True)
